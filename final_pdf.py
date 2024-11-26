64cc299ead2b6c2d50861738e63f71d9a99e4563
Manuel <nicolosi.1897040@studenti.uniroma1.it>
2024-11-26 16:56:33 +0100

    Final example - pdf

--- final_pdf.py
+++ final_pdf.py
@@ -17,7 +17,7 @@ import pandas as pd
 import re
 
 # Set our tracking server uri for logging
-mlflow.set_tracking_uri(uri="http://localhost:5000")
+#mlflow.set_tracking_uri(uri="http://localhost:5000")
 mlflow.set_experiment(experiment_name='Final example')
 mlflow.start_run(run_name='Planets and moons - PDF')
 run_id = mlflow.active_run().info.run_id
@@ -40,7 +40,7 @@ dataset_path = os.path.abspath("Dataset")
 # Impostare la connessione al database
 def connect_to_db():
     return psycopg2.connect(
-        host="13.51.172.225",  
+        host="16.171.25.235",  
         database="final_example",  
         user="postgres",  
         password="1234"  
